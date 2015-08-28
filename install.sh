ae933b1f98c0c3398f0032805c005232db62932a
Pascal Vervest <pascal@mail.pascalvervest.nl>
2015-08-28 10:08:46 +0200

    Correct path for symlinks

--- install.sh
+++ install.sh
@@ -3,14 +3,16 @@
 # Set homedirectory
 read -e -p "Set homedir: " -i "/home" HOMEDIR
 
+CURRENTDIR=${PWD}
+
 # Clone git-aware-prompt from github
 git clone https://github.com/jimeh/git-aware-prompt.git $HOMEDIR/.bash/git-aware-prompt
 
 # Set up symlinks
-ln -s bashrc $HOMEDIR/.bashrc
-ln -s bash_aliases $HOMEDIR/.bash_aliases
-ln -s gitconfig $HOMEDIR/.gitconfig
-ln -s gitignore $HOMEDIR/.gitignore
+ln -s -f $CURRENTDIR/bashrc $HOMEDIR/.bashrc
+ln -s -f $CURRENTDIR/bash_aliases $HOMEDIR/.bash_aliases
+ln -s -f $CURRENTDIR/gitconfig $HOMEDIR/.gitconfig
+ln -s -f $CURRENTDIR/gitignore $HOMEDIR/.gitignore
 
 echo '- - - - - - - '
 echo "All done! Review your settings, and do 'exec bash' to set up bash using the new settings"

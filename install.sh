f9d0e7b5d6476fbe34a88b974d89d880b2c147b0
Pascal Vervest <pascalv@sysadmin.prezent.nl>
2021-12-30 14:06:49 +0100

    Do not ask for homedir anymore, assume it's going to be installed in the current user homedir anyway

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 #/bin/bash
 
-# Set homedirectory
-read -e -p "Set homedir: " -i "/home" HOMEDIR
-
 CURRENTDIR=${PWD}
 
-# Clone git-aware-prompt from github
-git clone https://github.com/jimeh/git-aware-prompt.git $HOMEDIR/.bash/git-aware-prompt
+# create bash directory if it doesn't exist yet
+mkdir ~/.bash
 
+# Clone git-aware-prompt from github
+git clone https://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
 
 # Set up symlinks
-ln -s -f $CURRENTDIR/bashrc $HOMEDIR/.bashrc
-ln -s -f $CURRENTDIR/bash_preset_aliases $HOMEDIR/.bash_preset_aliases
-ln -s -f $CURRENTDIR/gitconfig $HOMEDIR/.gitconfig
-ln -s -f $CURRENTDIR/gitignore $HOMEDIR/.gitignore
+ln -s -f $CURRENTDIR/bashrc ~/.bashrc
+ln -s -f $CURRENTDIR/bash_preset_aliases ~/.bash_preset_aliases
+ln -s -f $CURRENTDIR/gitconfig ~/.gitconfig
+ln -s -f $CURRENTDIR/gitignore ~/.gitignore
 
 echo '- - - - - - - '
 echo "All done! Review your settings, and do 'exec bash' to set up bash using the new settings"
